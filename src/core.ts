ef56a8b176994038b758c01b04bfc8b39da8d47b
xxx <humengmingxx@gmail.com>
2022-06-16 23:39:02 +0800

    feat(*)：initially completed

--- src/core.ts
+++ src/core.ts
@@ -1,37 +1,82 @@
-class _PetiteDB {
-  constructor() { }
+class PetiteDB {
+  #db: any
+  #storeName = 'petiteStore'
 
-  /**
-   * 初始化
-   */
-  init() { }
+  constructor(dbName: string, storeName: string) {
+    this.#init(dbName, storeName)
+  }
 
-  /**
-   * 添加数据
-   */
-  setItem() { }
+  #init(dbName = 'petiteDB', storeName = 'petiteStore') {
+    return new Promise((resolve, reject) => {
+      if (window.indexedDB) {
+        this.#db = window.indexedDB.open(dbName, 1)
+        this.#storeName = storeName
+        this.#db.onupgradeneeded = (e: any) => {
+          this.#db = e.target.result
+          if (!this.#db.objectStoreNames.contains(storeName)) {
+            this.#db.createObjectStore(storeName, {
+              keyPath: 'key',
+            })
+          }
+        }
+        this.#db.onsuccess = (e: any) => {
+          this.#db = e.target.result
+          resolve(this)
+        }
+        this.#db.onerror = (e: unknown) => reject(e)
+      }
+    })
+  }
 
-  /**
-   * 获取数据
-   */
-  getItem() { }
+  setItem(key: string, value: any) {
+    return new Promise((resolve, reject) => {
+      const transaction = this.#db.transaction([this.#storeName], 'readwrite')
+      const store = transaction.objectStore(this.#storeName)
+      const request = store.put({ key, value })
+      request.onsuccess = (e: unknown) => resolve(e)
+      request.onerror = (e: unknown) => reject(e)
+    })
+  }
 
-  /**
-   * 删除数据
-   */
-  removeItem() { }
+  getItem(key: string) {
+    return new Promise((resolve, reject) => {
+      const transaction = this.#db.transaction([this.#storeName], 'readonly')
+      const store = transaction.objectStore(this.#storeName)
+      const request = store.get(key)
+      request.onsuccess = (e: any) => resolve(e.target.result)
+      request.onerror = (e: unknown) => reject(e)
+    })
+  }
 
-  /**
-   * 清空数据
-   */
-  clear() { }
+  removeItem(key: string) {
+    return new Promise((resolve, reject) => {
+      const transaction = this.#db.transaction([this.#storeName], 'readwrite')
+      const store = transaction.objectStore(this.#storeName)
+      const request = store.delete(key)
+      request.onsuccess = (e: unknown) => resolve(e)
+      request.onerror = (e: unknown) => reject(e)
+    })
+  }
 
-  /**
-   * 获取键值
-   */
-  key() { }
+  clear() {
+    return new Promise((resolve, reject) => {
+      const transaction = this.#db.transaction([this.#storeName], 'readwrite')
+      const store = transaction.objectStore(this.#storeName)
+      const request = store.clear()
+      request.onsuccess = (e: unknown) => resolve(e)
+      request.onerror = (e: unknown) => reject(e)
+    })
+  }
+
+  keys() {
+    return new Promise((resolve, reject) => {
+      const transaction = this.#db.transaction([this.#storeName], 'readonly')
+      const store = transaction.objectStore(this.#storeName)
+      const request = store.getAllKeys()
+      request.onsuccess = (e: any) => resolve(e.target.result)
+      request.onerror = (e: unknown) => reject(e)
+    })
+  }
 }
-
-const PetiteDB = new _PetiteDB()
 
 export default PetiteDB
